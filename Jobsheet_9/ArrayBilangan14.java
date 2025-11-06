6ccabef9616ca72ccf73fe18db4bb364fcc42142
fami0110 <masandofami@gmail.com>
2025-11-06 09:56:25 +0700

    feat: jobsheet 9 - percobaan 1

---
+++ Jobsheet_9/ArrayBilangan14.java
@@ -0,0 +1,9 @@
+public class ArrayBilangan14 {
+    public static void main(String[] args) {
+        int bil[] = {5, 13, -7, 17};
+
+        for (int i = 0; i <= bil.length; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
