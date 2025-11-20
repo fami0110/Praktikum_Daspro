a394170ea8a239773efa7acbfaf679dfdcd553f3
fami0110 <masandofami@gmail.com>
2025-11-20 10:32:20 +0700

    feat: kuis 2 - soal 1

---
+++ Kuis_2/Soal114.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Soal114 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jml_elemen, jml_ganjil = 0, jml_genap = 0;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        jml_elemen = sc.nextInt();
+        
+        int bilangan[] = new int[jml_elemen];
+
+        System.out.println("=============================");
+        
+        for (int i = 0; i < bilangan.length; i++) {
+            System.out.print("Masukkan nilai elemen ke-"+(i+1)+": ");
+            bilangan[i] = sc.nextInt();
+
+            if (bilangan[i] % 2 == 0) {
+                jml_genap++;
+            } else {
+                jml_ganjil++;
+            }
+        }
+        
+        System.out.println("=============================");
+
+        System.out.println("Jumlah semua elemen: " + bilangan.length);
+        System.out.println("Jumlah elemen ganjil: " + jml_ganjil);
+        System.out.println("Jumlah elemen genap: " + jml_genap);
+
+        sc.close();
+    }
+}
